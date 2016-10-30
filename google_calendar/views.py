0369768f2d9aab450f01200fadcd543bb6188379
root <CoralGift.N.H@uec.ac.jp>
2016-10-30 13:48:27 +0900

    commit46

--- google_calendar/views.py
+++ google_calendar/views.py
@@ -91,10 +91,11 @@ def add_schedule(key,schedule_body,apo_date):
     schedule_id = 'CoralGift.N.H@gmail.com'
     SCHEDULE_ENDPOINT = 'https://www.googleapis.com/calendar/v3/calendars/' + schedule_id + '/events?key=' + key
     payload = {
+        'key':key,
         'start':apo_date,
         'summary':schedule_body,
     }
-    response = requests.post(SCHEDULE_ENDPOINT,params=payload)
+    response = requests.post(SCHEDULE_ENDPOINT,data=json.dumps(payload))
     print(response)
     print(response.json())
     return response
