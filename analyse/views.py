999a457832adaea195d663616a319f0e08d2ed5c
root <CoralGift.N.H@uec.ac.jp>
2016-10-30 14:22:25 +0900

    commit50

--- analyse/views.py
+++ analyse/views.py
@@ -114,6 +114,7 @@ def docomo_api(text):
                 japan_date = re.sub("T",'',japan_date,1)
                 japan_date += '分'
         post_messages = japan_day + japan_date + japan_hour + 'のスケジュールはこちらです。'
+        key = 'ya29.Ci-KAwh4SLjr9QtjyC9H-K999j8A13OQtO9XyVBgDDKWrwil0P3oebmh5cXmRclSJw'
         schedule_data = check_schedule(key,schedule_date)
         message = post_messages + '\n' + schedule_data
         print(schedule_data)
